9e9bca1af24eb04de2a7b411414d5608eba6bf09
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-02 19:12:25 -0600

    Update simple_flash.c

--- decoder/src/simple_flash.c
+++ decoder/src/simple_flash.c
@@ -10,3 +10,8 @@ void flash_simple_read(uint32_t address, void* buffer, uint32_t size) {
 int flash_simple_write(uint32_t address, void* buffer, uint32_t size) {
     return MXC_FLC_Write(address, size, (uint32_t *)buffer);
 }
+
+/** @brief Securely erase a flash page before overwriting */
+int flash_simple_erase_page(uint32_t address) {
+    return MXC_FLC_PageErase(address);
+}
