447470fca6f024a2ed4b63f38f9cb449d385d448
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-02 19:11:51 -0600

    Update simple_uart.c

--- decoder/src/simple_uart.c
+++ decoder/src/simple_uart.c
@@ -1,18 +1,35 @@
 #include "simple_uart.h"
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "uart.h"
+#include "nvic_table.h"
+#include "board.h"
+#include "mxc_device.h"
 
 /** @brief Secure UART Initialization */
 int uart_init(void){
-    return MXC_UART_Init(MXC_UART_GET_UART(CONSOLE_UART), UART_BAUD, MXC_UART_IBRO_CLK);
+    int ret = MXC_UART_Init(MXC_UART_GET_UART(CONSOLE_UART), UART_BAUD, MXC_UART_IBRO_CLK);
+    if (ret != E_NO_ERROR) {
+        printf("Error initializing UART: %d\n", ret);
+    }
+    return ret;
 }
 
 /** @brief Secure UART Read */
 int uart_readbyte(void){
-    return MXC_UART_ReadCharacter(MXC_UART_GET_UART(CONSOLE_UART));
+    int data = MXC_UART_ReadCharacter(MXC_UART_GET_UART(CONSOLE_UART));
+    return data;
 }
 
 /** @brief Secure UART Write */
 void uart_writebyte(uint8_t data) {
     while (MXC_UART_GET_UART(CONSOLE_UART)->status & MXC_F_UART_STATUS_TX_FULL) {}
     MXC_UART_GET_UART(CONSOLE_UART)->fifo = data;
+}
+
+/** @brief Securely flush UART buffers */
+void uart_flush(void){
+    MXC_UART_ClearRXFIFO(MXC_UART_GET_UART(CONSOLE_UART));
+    MXC_UART_ClearTXFIFO(MXC_UART_GET_UART(CONSOLE_UART));
 }
