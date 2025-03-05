932ddad3c19a24d60114be779bcc7465dbbedb0d
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-05 09:26:42 -0600

    Wolfssl simple_uart.c

--- decoder/src/simple_uart.c
+++ decoder/src/simple_uart.c
@@ -1,18 +1,32 @@
 #include "simple_uart.h"
-#include <stdio.h>
+#include "wolfssl/ssl.h"  // Include wolfSSL for AES encryption
 
-/** @brief Secure UART Initialization */
-int uart_init(void){
-    return MXC_UART_Init(MXC_UART_GET_UART(CONSOLE_UART), UART_BAUD, MXC_UART_IBRO_CLK);
+void uart_writebyte(uint8_t data, uint8_t *key) {
+    uint8_t encrypted_data[1];
+    uint8_t data_buf[1] = {data};
+
+    // Encrypt data before sending using AES from wolfSSL
+    WOLFSSL_AES_KEY aes_key;
+    wolfSSL_AES_set_encrypt_key(&aes_key, key, 32);  // AES-256
+    wolfSSL_AES_encrypt(&aes_key, data_buf, encrypted_data);
+
+    while (MXC_UART_GET_UART(CONSOLE_UART)->status & MXC_F_UART_STATUS_TX_FULL) {
+    }
+    MXC_UART_GET_UART(CONSOLE_UART)->fifo = encrypted_data[0];
 }
 
-/** @brief Secure UART Read */
-int uart_readbyte(void){
-    return MXC_UART_ReadCharacter(MXC_UART_GET_UART(CONSOLE_UART));
-}
+int uart_readbyte(uint8_t *key) {
+    uint8_t encrypted_data[1];
+    uint8_t decrypted_data[1];
 
-/** @brief Secure UART Write */
-void uart_writebyte(uint8_t data) {
-    while (MXC_UART_GET_UART(CONSOLE_UART)->status & MXC_F_UART_STATUS_TX_FULL) {}
-    MXC_UART_GET_UART(CONSOLE_UART)->fifo = data;
+    int data = MXC_UART_ReadCharacter(MXC_UART_GET_UART(CONSOLE_UART));
+
+    encrypted_data[0] = data;
+
+    // Decrypt data after receiving using AES from wolfSSL
+    WOLFSSL_AES_KEY aes_key;
+    wolfSSL_AES_set_decrypt_key(&aes_key, key, 32);  // AES-256
+    wolfSSL_AES_decrypt(&aes_key, encrypted_data, decrypted_data);
+
+    return decrypted_data[0];
 }
