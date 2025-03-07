03a5e2916a0bd88280b4245b87708d348d5b8240
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-07 10:37:36 -0600

    Create led.h OpenSSL

---
+++ decoder/inc/led.h
@@ -0,0 +1,16 @@
+#ifndef LED_H
+#define LED_H
+
+// Function to initialize the LED
+void led_init(void);
+
+// Function to turn the LED on
+void led_on(void);
+
+// Function to turn the LED off
+void led_off(void);
+
+// Function to toggle the LED
+void led_toggle(void);
+
+#endif // LED_H
