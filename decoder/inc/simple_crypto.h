9f0bc09666eec699dee906c798e197b09f229bfc
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-08 00:44:56 -0600

    Update simple_crypto.h WolfSSL

--- decoder/inc/simple_crypto.h
+++ decoder/inc/simple_crypto.h
@@ -1,13 +1,13 @@
 #ifndef ECTF_CRYPTO_H
 #define ECTF_CRYPTO_H
 
-#include <openssl/aes.h>
-#include <openssl/evp.h>
-#include <openssl/rand.h>
+#include <wolfssl/wolfcrypt/aes.h>
+#include <wolfssl/wolfcrypt/hash.h>
+#include <wolfssl/wolfcrypt/types.h>
 
 #define BLOCK_SIZE AES_BLOCK_SIZE
-#define KEY_SIZE 32  // AES-256 key size
-#define HASH_SIZE 32  // SHA-256 hash size
+#define KEY_SIZE 32  // AES-256 key size (32 bytes)
+#define HASH_SIZE 32 // Using SHA-256 (32 bytes output)
 
 int encrypt_sym(uint8_t *plaintext, size_t len, uint8_t *key, uint8_t *ciphertext);
 int decrypt_sym(uint8_t *ciphertext, size_t len, uint8_t *key, uint8_t *plaintext);
