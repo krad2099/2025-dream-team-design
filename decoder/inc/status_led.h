2a4af03134f618c19497fb50d3cedd2ad34a1fdc
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-08 00:46:18 -0600

    Update status_led.h WolfSSL

--- decoder/inc/status_led.h
+++ decoder/inc/status_led.h
@@ -3,14 +3,19 @@
 
 #include "led.h"
 
+// Reset LED state
 #define STATUS_LED_OFF(void) LED_Off(LED1); LED_Off(LED2); LED_Off(LED3);
+// Error state
 #define STATUS_LED_RED(void) STATUS_LED_OFF(); LED_On(LED1);
+// Waiting state
 #define STATUS_LED_GREEN(void) STATUS_LED_OFF(); LED_On(LED2);
 #define STATUS_LED_BLUE(void) STATUS_LED_OFF(); LED_On(LED3);
+// Combined states
 #define STATUS_LED_PURPLE(void) STATUS_LED_OFF(); LED_On(LED1); LED_On(LED3);
 #define STATUS_LED_CYAN(void) STATUS_LED_OFF(); LED_On(LED2); LED_On(LED3);
 #define STATUS_LED_YELLOW(void) STATUS_LED_OFF(); LED_On(LED1); LED_On(LED2);
 #define STATUS_LED_WHITE(void) STATUS_LED_OFF(); LED_On(LED1); LED_On(LED2); LED_On(LED3);
+
 #define STATUS_LED_ERROR STATUS_LED_RED
 
 #endif // __STATUS_LED__
