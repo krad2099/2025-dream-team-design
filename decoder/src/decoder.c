382a5d6a0e28f8de496086d6de471838e5ff2f90
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-08 22:26:44 -0600

    Update decoder.c

--- decoder/src/decoder.c
+++ decoder/src/decoder.c
@@ -4,11 +4,6 @@
  * @brief   eCTF Dream Team Decoder Design Implementation
  * @date    2025
  *
- * This source file is part of an example system for MITRE's 2025 Embedded System CTF (eCTF).
- * This code is being provided only for educational purposes for the 2025 MITRE eCTF competition,
- * and may not meet MITRE standards for quality. Use this code at your own risk!
- *
- * @copyright Copyright (c) 2025 The MITRE Corporation
  */
 
 /*********************** INCLUDES *************************/
