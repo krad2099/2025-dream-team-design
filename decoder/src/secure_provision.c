db27346379220cd070062827ef2748a425334c69
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-13 02:26:07 -0500

    Update secure_provision.c

--- decoder/src/secure_provision.c
+++ decoder/src/secure_provision.c
@@ -6,7 +6,13 @@
  *
  * For demonstration purposes, this function calls flash_simple_read.
  * In production, replace this with access to a secure/OTP flash region.
+ *
+ * @param address The address of the secure secret.
+ * @param buffer Pointer to the buffer where the secret will be stored.
+ * @param len Number of bytes to read.
+ * @return int 0 on success, negative on error.
  */
 int secure_flash_read(uint32_t address, uint8_t *buffer, size_t len) {
-    return flash_simple_read(address, buffer, len);
+    flash_simple_read(address, buffer, len);
+    return 0;
 }
