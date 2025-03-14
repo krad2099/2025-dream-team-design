e8097462a4820d64f1feebc796552d63aee6212d
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-13 23:36:56 -0500

    Update simple_flash.c

--- decoder/src/simple_flash.c
+++ decoder/src/simple_flash.c
@@ -1,7 +1,7 @@
 /**
- * @file "simple_flash.c"
+ * @file simple_flash.c
  * @author Dream Team
- * @brief Simple Flash Interface Implementation 
+ * @brief Simple Flash Interface Implementation for the MAX78000.
  * @date 2025
  *
  */
@@ -12,11 +12,7 @@
 #include "flc.h"
 #include "icc.h"
 #include "nvic_table.h"
-#include <stdio.h>
 
-/**
- * @brief ISR for the Flash Controller.
- */
 void flash_simple_irq(void) {
     uint32_t temp;
     temp = MXC_FLC0->intr;
@@ -31,9 +27,6 @@ void flash_simple_irq(void) {
     }
 }
 
-/**
- * @brief Initialize the Simple Flash Interface.
- */
 void flash_simple_init(void) {
     MXC_NVIC_SetVector(FLC0_IRQn, flash_simple_irq);
     NVIC_EnableIRQ(FLC0_IRQn);
@@ -41,30 +34,20 @@ void flash_simple_init(void) {
     MXC_ICC_Disable(MXC_ICC0);
 }
 
-/**
- * @brief Erase a flash page.
- */
 int flash_simple_erase_page(uint32_t address) {
     return MXC_FLC_PageErase(address);
 }
 
-/**
- * @brief Read data from flash.
- */
 void flash_simple_read(uint32_t address, void* buffer, uint32_t size) {
     MXC_FLC_Read(address, (uint32_t *)buffer, size);
 }
 
-/**
- * @brief Write data to flash.
- */
 int flash_simple_write(uint32_t address, void* buffer, uint32_t size) {
     return MXC_FLC_Write(address, size, (uint32_t *)buffer);
 }
 
-/**
- * @brief Load Global Secrets from a designated secure flash region.
- */
-/*void load_global_secret(uint8_t *secret_buffer, size_t len) {
-    flash_simple_read(SECRET_STORAGE_ADDR, secret_buffer, len);
-}*/
+// If load_global_secret is required, define it here (but only once).
+// For example, if secrets are stored at a fixed address:
+void load_global_secret(uint8_t *secret_buffer, size_t len) {
+    flash_simple_read(SECRET_STORAGE_ADDR, secret_buffer, (uint32_t)len);
+}
