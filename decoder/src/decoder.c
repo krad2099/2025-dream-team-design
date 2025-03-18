491f815cfa407aa2cb8e0e42912bc3739dd13410
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-18 11:33:31 -0500

    Update decoder.c

--- decoder/src/decoder.c
+++ decoder/src/decoder.c
@@ -8,12 +8,12 @@
 #include "simple_flash.h"
 #include "host_messaging.h"
 #include "simple_uart.h"
-#include "simple_crypto.h"
+#include "simple_crypto.h"  // Added for AES decryption
+#include "subscription.h"   // Ensure is_subscribed() is declared
 
 /**********************************************************
  ******************* PRIMITIVE TYPES **********************
  **********************************************************/
-
 #define timestamp_t uint64_t
 #define channel_id_t uint32_t
 #define decoder_id_t uint32_t
@@ -22,12 +22,12 @@
 /**********************************************************
  *********************** CONSTANTS ************************
  **********************************************************/
-
 #define MAX_CHANNEL_COUNT 8
 #define EMERGENCY_CHANNEL 0
 #define FRAME_SIZE 64
 #define DEFAULT_CHANNEL_TIMESTAMP 0xFFFFFFFFFFFFFFFF
 #define FLASH_FIRST_BOOT 0xDEADBEEF
+#define FLASH_STATUS_ADDR 0x100000  // Define address properly
 
 #define AES_KEY_SIZE 32
 #define AES_BLOCK_SIZE 16
@@ -35,7 +35,6 @@
 /**********************************************************
  ******************* STRUCT DEFINITIONS *******************
  **********************************************************/
-
 #pragma pack(push, 1)
 typedef struct {
     channel_id_t channel;
@@ -43,17 +42,23 @@ typedef struct {
     uint8_t iv[AES_BLOCK_SIZE];
     uint8_t encrypted_data[FRAME_SIZE];
 } frame_packet_t;
+
+typedef struct {
+    uint32_t first_boot;
+    channel_status_t subscribed_channels[MAX_CHANNEL_COUNT];
+} flash_entry_t;
+
 #pragma pack(pop)
 
 /**********************************************************
  ******************** GLOBAL VARIABLES ********************
  **********************************************************/
 static uint8_t encryption_key[AES_KEY_SIZE];
+flash_entry_t decoder_status;
 
 /**********************************************************
  ******************* UTILITY FUNCTIONS ********************
  **********************************************************/
-
 void decrypt_frame(uint8_t *ciphertext, uint8_t *plaintext, uint8_t *iv) {
     decrypt_sym(ciphertext, FRAME_SIZE, encryption_key, iv, plaintext);
 }
@@ -61,7 +66,6 @@ void decrypt_frame(uint8_t *ciphertext, uint8_t *plaintext, uint8_t *iv) {
 /**********************************************************
  ********************* CORE FUNCTIONS *********************
  **********************************************************/
-
 int decode(pkt_len_t pkt_len, frame_packet_t *new_frame) {
     char output_buf[128] = {0};
     uint8_t decrypted_frame[FRAME_SIZE];
@@ -99,10 +103,25 @@ int main(void) {
         msg_type_t cmd;
         uint16_t pkt_len;
         if (read_packet(&cmd, uart_buf, &pkt_len) < 0) continue;
+
         switch (cmd) {
-        case DECODE_MSG:
-            decode(pkt_len, (frame_packet_t *)uart_buf);
-            break;
+            case DECODE_MSG:
+                decode(pkt_len, (frame_packet_t *)uart_buf);
+                break;
+            case SUBSCRIBE_MSG:
+                update_subscription(pkt_len, (subscription_update_packet_t *)uart_buf);
+                break;
+            case LIST_MSG:
+                list_channels();
+                break;
+            case ACK_MSG:
+            case DEBUG_MSG:
+            case ERROR_MSG:
+            case ENCRYPTED_MSG:
+                print_debug("Unhandled message type\n");
+                break;
+            default:
+                print_error("Unknown command received\n");
         }
     }
 }
