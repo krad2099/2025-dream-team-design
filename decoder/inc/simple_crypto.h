5a6ddaed82f0c6ab0318c56e5163933550514ca7
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-18 11:34:26 -0500

    Update simple_crypto.h

--- decoder/inc/simple_crypto.h
+++ decoder/inc/simple_crypto.h
@@ -1,4 +1,3 @@
-#if CRYPTO_EXAMPLE 
 #ifndef ECTF_CRYPTO_H
 #define ECTF_CRYPTO_H
 
@@ -7,56 +6,13 @@
 
 /******************************** MACRO DEFINITIONS ********************************/
 #define BLOCK_SIZE AES_BLOCK_SIZE
-#define KEY_SIZE 32
-#define IV_SIZE AES_BLOCK_SIZE
+#define KEY_SIZE 32            // Updated to AES-256 key size
+#define IV_SIZE AES_BLOCK_SIZE  // Added for CBC mode
 #define HASH_SIZE MD5_DIGEST_SIZE
 
 /******************************** FUNCTION PROTOTYPES ********************************/
-/** @brief Encrypts plaintext using AES-256 in CBC mode with an IV
- *
- * @param plaintext A pointer to a buffer of length len containing the
- *          plaintext to encrypt
- * @param len The length of the plaintext to encrypt. Must be a multiple of
- *          BLOCK_SIZE (16 bytes)
- * @param key A pointer to a buffer of length KEY_SIZE (32 bytes) containing
- *          the key to use for encryption
- * @param iv A pointer to a buffer of IV_SIZE (16 bytes) containing
- *          the initialization vector for CBC mode
- * @param ciphertext A pointer to a buffer of length len where the resulting
- *          ciphertext will be written to
- *
- * @return 0 on success, -1 on bad length, other non-zero for other errors
- */
 int encrypt_sym(uint8_t *plaintext, size_t len, uint8_t *key, uint8_t *iv, uint8_t *ciphertext);
-
-/** @brief Decrypts ciphertext using AES-256 in CBC mode with an IV
- *
- * @param ciphertext A pointer to a buffer of length len containing the
- *           ciphertext to decrypt
- * @param len The length of the ciphertext to decrypt. Must be a multiple of
- *           BLOCK_SIZE (16 bytes)
- * @param key A pointer to a buffer of length KEY_SIZE (32 bytes) containing
- *           the key to use for decryption
- * @param iv A pointer to a buffer of IV_SIZE (16 bytes) containing
- *           the initialization vector used during encryption
- * @param plaintext A pointer to a buffer of length len where the resulting
- *           plaintext will be written to
- *
- * @return 0 on success, -1 on bad length, other non-zero for other errors
- */
 int decrypt_sym(uint8_t *ciphertext, size_t len, uint8_t *key, uint8_t *iv, uint8_t *plaintext);
-
-/** @brief Hashes arbitrary-length data
- *
- * @param data A pointer to a buffer of length len containing the data
- *           to be hashed
- * @param len The length of the plaintext to hash
- * @param hash_out A pointer to a buffer of length HASH_SIZE (16 bytes) where the resulting
- *           hash output will be written to
- *
- * @return 0 on success, non-zero for other errors
- */
 int hash(void *data, size_t len, uint8_t *hash_out);
 
-#endif // CRYPTO_EXAMPLE
 #endif // ECTF_CRYPTO_H
