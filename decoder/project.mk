0be13da16b42a03a6de521b35868a56e6e92228e
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-04-13 23:33:45 -0500

    Update project.mk

--- decoder/project.mk
+++ decoder/project.mk
@@ -46,6 +46,8 @@ CRYPTO_EXAMPLE=1
 # Include the WolfSSL headers
 CFLAGS += -Iinc/wolfssl  
 
+PROJ_LDFLAGS += -lwolfssl
+
 # Ensure the library path points to the correct directory
 LIBPATH += ./inc/wolfssl/src/.libs  # Update this path to where your libwolfssl.a or libwolfssl.so is located
 
